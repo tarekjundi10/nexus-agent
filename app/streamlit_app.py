3741d6bb3622d98aadb6165287ba500ae5ca4587
tarekjundi10 <tarekjbs1@gmail.com>
2026-04-13 17:01:52 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,7 +6,7 @@ API_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(
     page_title="Nexus Agent",
-    page_icon="assets/favicon.png",
+    page_icon="../assets/favicon.png",
     layout="wide"
 )
 
